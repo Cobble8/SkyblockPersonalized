ec032832e131d1521583787251592b29775f4698
Cobble8 <41165207+Cobble8@users.noreply.github.com>
2020-11-23 02:44:45 -0500

    Add files via upload

--- main/java/cobble/sbp/SBP.java
+++ main/java/cobble/sbp/SBP.java
@@ -4,27 +4,37 @@ import java.io.IOException;
 
 import com.mojang.realmsclient.gui.ChatFormatting;
 
+import cobble.sbp.commands.CheckLatestVersion;
 import cobble.sbp.commands.Dungeons;
 import cobble.sbp.commands.EnableMod;
+import cobble.sbp.commands.IcePathPuzzle;
 import cobble.sbp.commands.SetAPIKey;
+import cobble.sbp.commands.ShowConfig;
+import cobble.sbp.commands.SilverfishPuzzle;
 import cobble.sbp.handlers.ConfigHandler;
 import cobble.sbp.handlers.KeyBindingHandler;
 import cobble.sbp.handlers.KeyInputHandler;
 import cobble.sbp.handlers.RenderGuiHandler;
 import cobble.sbp.simplejson.parser.ParseException;
+import cobble.sbp.utils.DataGetter;
 import cobble.sbp.utils.HttpClient;
 import cobble.sbp.utils.Reference;
 import cobble.sbp.utils.Utils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 
@@ -38,29 +48,21 @@ public class SBP
 		ConfigHandler.registerConfig(event);
 		//Commands
 
-		//ClientCommandHandler.instance.registerCommand(new ShowConfig());
-		//ClientCommandHandler.instance.registerCommand(new TaskList());
 		ClientCommandHandler.instance.registerCommand(new SetAPIKey());
-		//ClientCommandHandler.instance.registerCommand(new SBPMain());
 		ClientCommandHandler.instance.registerCommand(new EnableMod());
 		ClientCommandHandler.instance.registerCommand(new Dungeons());
+		ClientCommandHandler.instance.registerCommand(new ShowConfig());
+		ClientCommandHandler.instance.registerCommand(new CheckLatestVersion());
+		ClientCommandHandler.instance.registerCommand(new SilverfishPuzzle());
+		ClientCommandHandler.instance.registerCommand(new IcePathPuzzle());
 		
 		KeyBindingHandler.register();
 		MinecraftForge.EVENT_BUS.register(new KeyInputHandler());
 		
 		
+		
+		
 	}
-	
-	/*@SubscribeEvent
-	public void onChat(ClientChatReceivedEvent event) {
-	    String message = event.message.getUnformattedText();
-	    
-	        if(message.contains("API")) {
-	        		message.replace("Your new API key is ", "");
-	                 Utils.sendMessage(ChatFormatting.GREEN+"[SBP] Your Hypixel API Key has been saved!");
-	                 try {ConfigHandler.newObject("APIKey", message);} catch (IOException e) {e.printStackTrace();}
-	        }
-	}*/
 	
 	@EventHandler
 	public static void init(FMLInitializationEvent event)
@@ -73,20 +75,11 @@ public class SBP
 	public static void postInit(FMLPostInitializationEvent event) throws Exception
 	{
 		MinecraftForge.EVENT_BUS.register(new RenderGuiHandler());
-	
+		
 		 
 	}
-	
-	/*@EventHandler
-	public static void loginEvent(PlayerLoggedInEvent event) {
-		if(firstLaunch == true) {
-			Utils.sendMessage(ChatFormatting.GOLD+"Thank you for downloading SkyblockPersonalized!");
-			Utils.sendMessage(ChatFormatting.GOLD+"If you have any problems with the mod be sure to DM "+ChatFormatting.BLUE+"Cobble8#0881"+ChatFormatting.GOLD+" on "+ChatFormatting.BLUE+"Discord!");
-			Utils.sendMessage(ChatFormatting.GOLD+"This message has appeared because this is either your first launch or you deleted your config file!");
-			Utils.sendMessage("Make sure to set your "+ChatFormatting.RED+"API Key"+ChatFormatting.GOLD+" by typing: "+ChatFormatting.YELLOW+"/api new"+ChatFormatting.GOLD+" and using the key given type: "+ChatFormatting.YELLOW+"/sbpsetkey [Your Api Key]"+ChatFormatting.GOLD+"!");
-			firstLaunch = false;
-		}
-	}*/
+
+
 	
 	@EventHandler
 	public void serverInit(FMLServerStartingEvent event)
