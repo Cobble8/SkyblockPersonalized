6b1ce020e2c8082d3042f0ce5e70ffb69f2ae4af
nea <nea@nea.moe>
2023-02-23 16:06:02 +0100

    Block axe warnings

--- src/main/java/com/cobble/sbp/utils/Reference.java
+++ src/main/java/com/cobble/sbp/utils/Reference.java
@@ -2,7 +2,7 @@ package com.cobble.sbp.utils;
 
 public class Reference {
     public static final String MODID = "sbp";
-    public static final String VERSION = "1.7.0.0";
+    public static final String VERSION = "1.7.1.0";
     public static final String MOD_STATE = "Full Release";
     public static final String GAME_VERSION = "1.8.9";
     public static final String NAME = "Skyblock Personalized";
